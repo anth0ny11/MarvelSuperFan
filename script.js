10b35832a237b0c0ebd590b28111b0607e65b9e3
anth0ny11 <anthony_trejo@student.davincischools.org>
2025-12-16 21:28:47 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,225 @@
+function spiderMan() {
+  document.getElementById("changingimage").src = "images/spidermanfirsttt.jpg";
+  document.getElementById("parent1").classList.remove("translucent");
+  document.getElementById("parent2").classList.add("translucent");
+  document.getElementById("parent3").classList.add("translucent");
+  document.getElementById("parent4").classList.add("translucent");
+  document.getElementById("parent5").classList.add("translucent");
+  document.getElementById("parent6").classList.add("translucent");
+  document.getElementById("parent7").classList.add("translucent");
+  document.getElementById("parent8").classList.add("translucent");
+  document.getElementById("parent9").classList.add("translucent");
+  document.getElementById("parent1").classList.remove("black");
+  document.getElementById("parent1").classList.add("red");
+  document.getElementById("parent2").classList.remove("grey");
+  document.getElementById("parent3").classList.remove("red");
+  document.getElementById("parent4").classList.remove("red");
+  document.getElementById("parent5").classList.remove("darkblue");
+  document.getElementById("parent6").classList.remove("purple");
+  document.getElementById("parent7").classList.remove("blue");
+  document.getElementById("parent8").classList.remove("darkblue");
+  document.getElementById("parent9").classList.remove("red");
+  document.getElementById("my_click").play();
+  document.getElementById("changingbio").innerHTML =
+    "Spider-Man, created by Stan Lee and Steve Ditko, is the superhero identity of Peter Parker, a teenager who gains spider-like abilities after being bitten by a radioactive spider. He learns the importance of responsibility after his Uncle Ben is killed by a criminal Peter once ignored. Spider-Man first appeared in Amazing Fantasy #15 in August 1962.";
+}
+function hulk() {
+  document.getElementById("changingimage").src = "images/hulkfirst.jpg";
+  document.getElementById("parent2").classList.remove("translucent");
+  document.getElementById("parent1").classList.add("translucent");
+  document.getElementById("parent3").classList.add("translucent");
+  document.getElementById("parent4").classList.add("translucent");
+  document.getElementById("parent5").classList.add("translucent");
+  document.getElementById("parent6").classList.add("translucent");
+  document.getElementById("parent7").classList.add("translucent");
+  document.getElementById("parent8").classList.add("translucent");
+  document.getElementById("parent9").classList.add("translucent");
+  document.getElementById("parent2").classList.remove("black");
+  document.getElementById("parent2").classList.add("grey");
+  document.getElementById("parent1").classList.remove("red");
+  document.getElementById("parent3").classList.remove("red");
+  document.getElementById("parent4").classList.remove("red");
+  document.getElementById("parent5").classList.remove("darkblue");
+  document.getElementById("parent6").classList.remove("purple");
+  document.getElementById("parent7").classList.remove("blue");
+  document.getElementById("parent8").classList.remove("darkblue");
+  document.getElementById("parent9").classList.remove("red");
+  document.getElementById("my_click").play();
+  document.getElementById("changingbio").innerHTML =
+    "The Hulk is the powerful alter ego of Dr. Bruce Banner, who transforms into a giant green being with immense strength whenever he becomes angry. He first appeared in The Incredible Hulk #1 in 1962 and represents the battle between human restraint and uncontrollable rage.";
+}
+function thor() {
+  document.getElementById("changingimage").src = "images/thorfirst.jpg";
+  document.getElementById("parent3").classList.remove("translucent");
+  document.getElementById("parent1").classList.add("translucent");
+  document.getElementById("parent2").classList.add("translucent");
+  document.getElementById("parent4").classList.add("translucent");
+  document.getElementById("parent5").classList.add("translucent");
+  document.getElementById("parent6").classList.add("translucent");
+  document.getElementById("parent7").classList.add("translucent");
+  document.getElementById("parent8").classList.add("translucent");
+  document.getElementById("parent9").classList.add("translucent");
+  document.getElementById("parent3").classList.remove("black");
+  document.getElementById("parent3").classList.add("red");
+  document.getElementById("parent1").classList.remove("red");
+  document.getElementById("parent2").classList.remove("grey");
+  document.getElementById("parent4").classList.remove("red");
+  document.getElementById("parent5").classList.remove("darkblue");
+  document.getElementById("parent6").classList.remove("purple");
+  document.getElementById("parent7").classList.remove("blue");
+  document.getElementById("parent8").classList.remove("darkblue");
+  document.getElementById("parent9").classList.remove("red");
+  document.getElementById("my_click").play();
+  document.getElementById("changingbio").innerHTML =
+    "Thor is a Marvel Comics superhero based on the Norse god of thunder, created by Stan Lee, Larry Lieber, and Jack Kirby. He first appeared in Journey into Mystery #83 (1962) and is known for wielding the magical hammer Mjolnir, granting him control over lightning, flight, and immense strength.";
+}
+function ironman() {
+  document.getElementById("changingimage").src = "images/ironmanfirst.jpg";
+  document.getElementById("parent4").classList.remove("translucent");
+  document.getElementById("parent1").classList.add("translucent");
+  document.getElementById("parent2").classList.add("translucent");
+  document.getElementById("parent3").classList.add("translucent");
+  document.getElementById("parent5").classList.add("translucent");
+  document.getElementById("parent6").classList.add("translucent");
+  document.getElementById("parent7").classList.add("translucent");
+  document.getElementById("parent8").classList.add("translucent");
+  document.getElementById("parent9").classList.add("translucent");
+  document.getElementById("parent4").classList.remove("black");
+  document.getElementById("parent4").classList.add("red");
+  document.getElementById("parent1").classList.remove("red");
+  document.getElementById("parent2").classList.remove("grey");
+  document.getElementById("parent3").classList.remove("red");
+  document.getElementById("parent5").classList.remove("darkblue");
+  document.getElementById("parent6").classList.remove("purple");
+  document.getElementById("parent7").classList.remove("blue");
+  document.getElementById("parent8").classList.remove("darkblue");
+  document.getElementById("parent9").classList.remove("red");
+  document.getElementById("my_click").play();
+  document.getElementById("changingbio").innerHTML =
+    "Iron Man is a Marvel superhero created by Stan Lee, Larry Lieber, Don Heck, and Jack Kirby, centered on genius billionaire Tony Stark, who builds a high-tech armored suit to save his life and fight threats. He first appeared in Tales of Suspense #39 in 1963.";
+}
+function b() {
+  document.getElementById("changingimage").src = "images/blackpantherfirst.jpg";
+  document.getElementById("parent5").classList.remove("translucent");
+  document.getElementById("parent1").classList.add("translucent");
+  document.getElementById("parent2").classList.add("translucent");
+  document.getElementById("parent3").classList.add("translucent");
+  document.getElementById("parent4").classList.add("translucent");
+  document.getElementById("parent6").classList.add("translucent");
+  document.getElementById("parent7").classList.add("translucent");
+  document.getElementById("parent8").classList.add("translucent");
+  document.getElementById("parent9").classList.add("translucent");
+  document.getElementById("parent5").classList.remove("black");
+  document.getElementById("parent5").classList.add("darkblue");
+  document.getElementById("parent1").classList.remove("red");
+  document.getElementById("parent2").classList.remove("grey");
+  document.getElementById("parent3").classList.remove("red");
+  document.getElementById("parent4").classList.remove("red");
+  document.getElementById("parent6").classList.remove("purple");
+  document.getElementById("parent7").classList.remove("blue");
+  document.getElementById("parent8").classList.remove("darkblue");
+  document.getElementById("parent9").classList.remove("red");
+  document.getElementById("my_click").play();
+  document.getElementById("changingbio").innerHTML =
+    "Black Panther is a Marvel superhero and the king of Wakanda, known as T’Challa, who gains enhanced abilities from the Heart-Shaped Herb and uses advanced Wakandan technology to protect his people. He first appeared in Fantastic Four #52 in 1966, becoming the first Black superhero in mainstream American comics.";
+}
+function hawkeye() {
+  document.getElementById("changingimage").src = "images/hawkeyefirst.jpg";
+  document.getElementById("parent6").classList.remove("translucent");
+  document.getElementById("parent1").classList.add("translucent");
+  document.getElementById("parent2").classList.add("translucent");
+  document.getElementById("parent3").classList.add("translucent");
+  document.getElementById("parent4").classList.add("translucent");
+  document.getElementById("parent5").classList.add("translucent");
+  document.getElementById("parent7").classList.add("translucent");
+  document.getElementById("parent8").classList.add("translucent");
+  document.getElementById("parent9").classList.add("translucent");
+  document.getElementById("parent6").classList.remove("black");
+  document.getElementById("parent6").classList.add("purple");
+  document.getElementById("parent1").classList.remove("red");
+  document.getElementById("parent2").classList.remove("grey");
+  document.getElementById("parent3").classList.remove("red");
+  document.getElementById("parent4").classList.remove("red");
+  document.getElementById("parent5").classList.remove("darkblue");
+  document.getElementById("parent7").classList.remove("blue");
+  document.getElementById("parent8").classList.remove("darkblue");
+  document.getElementById("parent9").classList.remove("red");
+  document.getElementById("my_click").play();
+  document.getElementById("changingbio").innerHTML =
+    "Hawkeye, also known as Clint Barton, is a former circus performer who honed his archery skills with almost superhuman precision, eventually becoming one of the world’s greatest marksmen. After a troubled early life, he joins the Avengers, where his quick wit, loyalty, and unmatched accuracy make him a valuable member despite having no superhuman powers.";
+}
+function captainamerica() {
+  document.getElementById("changingimage").src = "images/capfirst.jpg";
+  document.getElementById("parent7").classList.remove("translucent");
+  document.getElementById("parent1").classList.add("translucent");
+  document.getElementById("parent2").classList.add("translucent");
+  document.getElementById("parent3").classList.add("translucent");
+  document.getElementById("parent4").classList.add("translucent");
+  document.getElementById("parent5").classList.add("translucent");
+  document.getElementById("parent6").classList.add("translucent");
+  document.getElementById("parent8").classList.add("translucent");
+  document.getElementById("parent9").classList.add("translucent");
+  document.getElementById("parent7").classList.remove("black");
+  document.getElementById("parent7").classList.add("blue");
+  document.getElementById("parent1").classList.remove("red");
+  document.getElementById("parent2").classList.remove("grey");
+  document.getElementById("parent3").classList.remove("red");
+  document.getElementById("parent4").classList.remove("red");
+  document.getElementById("parent5").classList.remove("darkblue");
+  document.getElementById("parent6").classList.remove("purple");
+  document.getElementById("parent8").classList.remove("darkblue");
+  document.getElementById("parent9").classList.remove("red");
+  document.getElementById("my_click").play();
+  document.getElementById("changingbio").innerHTML =
+    "Captain America, whose real name is Steve Rogers, is a super-soldier created during World War II to fight tyranny, enhanced to peak human strength, agility, and endurance by the Super-Soldier Serum. He wields an indestructible shield as both a weapon and defense, symbolizing justice and freedom, and first appeared in Captain America Comics #1 in 1941.";
+}
+function widow() {
+  document.getElementById("changingimage").src = "images/blackwidowfirst.jpg";
+  document.getElementById("parent8").classList.remove("translucent");
+  document.getElementById("parent1").classList.add("translucent");
+  document.getElementById("parent2").classList.add("translucent");
+  document.getElementById("parent3").classList.add("translucent");
+  document.getElementById("parent4").classList.add("translucent");
+  document.getElementById("parent5").classList.add("translucent");
+  document.getElementById("parent6").classList.add("translucent");
+  document.getElementById("parent7").classList.add("translucent");
+  document.getElementById("parent9").classList.add("translucent");
+  document.getElementById("parent8").classList.remove("black");
+  document.getElementById("parent8").classList.add("darkblue");
+  document.getElementById("parent1").classList.remove("red");
+  document.getElementById("parent2").classList.remove("grey");
+  document.getElementById("parent3").classList.remove("red");
+  document.getElementById("parent4").classList.remove("red");
+  document.getElementById("parent5").classList.remove("darkblue");
+  document.getElementById("parent6").classList.remove("purple");
+  document.getElementById("parent7").classList.remove("blue");
+  document.getElementById("parent9").classList.remove("red");
+  document.getElementById("my_click").play();
+  document.getElementById("changingbio").innerHTML =
+    "Black Widow is a highly trained spy and assassin named Natasha Romanoff, known for her mastery of combat, espionage, and agility rather than superpowers. She first appeared in Tales of Suspense #52 in 1964.";
+}
+function strange() {
+  document.getElementById("changingimage").src = "images/strangefirst.jpg";
+  document.getElementById("parent9").classList.remove("translucent");
+  document.getElementById("parent1").classList.add("translucent");
+  document.getElementById("parent2").classList.add("translucent");
+  document.getElementById("parent3").classList.add("translucent");
+  document.getElementById("parent4").classList.add("translucent");
+  document.getElementById("parent5").classList.add("translucent");
+  document.getElementById("parent6").classList.add("translucent");
+  document.getElementById("parent7").classList.add("translucent");
+  document.getElementById("parent8").classList.add("translucent");
+  document.getElementById("parent9").classList.remove("black");
+  document.getElementById("parent9").classList.add("red");
+  document.getElementById("parent1").classList.remove("red");
+  document.getElementById("parent2").classList.remove("grey");
+  document.getElementById("parent3").classList.remove("red");
+  document.getElementById("parent4").classList.remove("red");
+  document.getElementById("parent5").classList.remove("darkblue");
+  document.getElementById("parent6").classList.remove("purple");
+  document.getElementById("parent7").classList.remove("blue");
+  document.getElementById("parent8").classList.remove("darkblue");
+  document.getElementById("my_click").play();
+  document.getElementById("changingbio").innerHTML =
+    "Doctor Strange, also known as Stephen Strange, is a former neurosurgeon who becomes the Sorcerer Supreme, protecting Earth from mystical and interdimensional threats. After a car accident ruins his hands, he learns magic from the Ancient One and first appeared in Strange Tales #110 in 1963.";
+}
